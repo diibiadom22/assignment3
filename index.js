7e4441b228a700b4181fb61ee48b32d4a10861b9
diibiadom22 <odiibiadom@gmail.com>
2024-09-10 04:38:52 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+//console.log(Math.round(4.4));
+//console.log(Math.round(4.5));
+// program showing local scope of a variable
+// let a = "hello";
+
+// function greet() {
+//     let b = "World"
+//     console.log(a + b);
+// }
+
+// greet();
+// console.log(a+b);
+
+// const person = {
+//     name: 'yewande',
+//     email: 'yewande@yewande.com',
+//     age: 25,
+//     gender: "female",
+//     profile: function() {
+//     console.log(this.name + ' is a ' + this.gender + ' and she is ' + this.age + ' years old!')
+//     }
+//     }
+//     person.occupation = "student"
+//     person.stature = "huge"
+//     person.complexsion = "dark"
+//     person.position = "tresuary"
+//     console.log(person)
+    
+// let example = ['fishing','eating','laughing','playing'];
+//  example[4] ='washing';
+//  console.log(example)
+
+//let studentsData = ['Jack','dancing','clapping', 5, 'sweeping', 3,'paul',50]
+   // ['Sara', 23, 'eating','johnson',70],
+    //['mary', 20, 'jumping','johny', 30],
+    //['martha',35, 'playing','junior',60]
+   // let remove = studentsData.splice(1,2,'apple','bread','tea','coke','fanta','beer');
+   // console.log(remove);
+   // console.log(studentsData);
+    
+   // let studentsData = [
+   //    ['Jack', 24], 
+   //    ['Sara', 23],
+   // ];
+   
+   // // iterating over the studentsData
+   // studentsData.forEach((student) => {
+   //     student.forEach((data) => {
+   //         console.log(data);
+   //     });
+   // });
+   
+   // const courses = [
+   //    {id: 1, name: 'html'},
+   //    {id: 2, name: 'css'},
+   //    {id: 3, name: 'javascript'},
+   //    ];
+   //    const course = courses.find((x) => {
+   //      return x.name === "javascript"
+   //    })
+   //    console.log(course);
+      
+   // const courses = [
+   //    {id: 1, name: "html"},
+   //    {id: 2, name: "css"},
+   //    {id: 3, name: "javascript"}
+   //    ]
+   //    courses.map((course) => {
+   //       console.log(course)
+   //    });
+      
